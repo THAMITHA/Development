7b0fd323abd3cb5daf884a305eebcefbc8870e83
THAMITHA S <THAMITHA.S@ibm.com>
2023-11-30 15:30:39 +0530

    add generic array

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-function LoggerAndReturn(thing) {
-    return thing;
-}
-//const message: string = LoggerAndReturn<string>('Hello World');
-var message = LoggerAndReturn('Hello World!!!');
-var message1 = LoggerAndReturn(2);
-console.log(message);
-console.log(message1);
+// -----------generic string------------------------
+
+// function LoggerAndReturn(thing) {
+//     return thing;
+// }
+// //const message: string = LoggerAndReturn<string>('Hello World');
+// var message = LoggerAndReturn('Hello World!!!');
+// var message1 = LoggerAndReturn(2);
+// console.log(message);
+// console.log(message1);
+
+
+
+// ---------generic arrays----------------------
+
+
+
+
+
+
+
--- app.ts
+++ app.ts
@@ -1,10 +1,28 @@
-function LoggerAndReturn<T>(thing: T) : T { 
-    return thing;
+// -----------generic String------------------------
+
+
+// function LoggerAndReturn<T>(thing: T) : T { 
+//     return thing;
+// }
+
+// //const message: string = LoggerAndReturn<string>('Hello World');
+// const message: string = LoggerAndReturn('Hello World!!!');
+// const message1: number = LoggerAndReturn(2);
+
+// console.log(message);
+// console.log(message1);
+
+
+
+// -----------Generic Array------------------------
+
+function getArray<T>(items: T[]): T[]{
+    return new Array<T>().concat(items);
 }
 
-//const message: string = LoggerAndReturn<string>('Hello World');
-const message: string = LoggerAndReturn('Hello World!!!');
-const message1: number = LoggerAndReturn(2);
 
-console.log(message);
-console.log(message1);
+let myNumArr = getArray<number>([100,200,300]);
+let myStrArr = getArray<string>(['Hello', 'World']);
+
+console.log(myNumArr);
+console.log(myStrArr);
